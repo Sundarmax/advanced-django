e7e8bdfce654bf7f8dc0d2927c797326c3084516
sundarmax <sundar.info22@gmail.com>
2020-06-08 23:06:27 +0530

    class based views updates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+
+from .models import Question
+
+class IndexView(generic.ListView):
+    template_name       = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('id')
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class DeleteView(generic.DeleteView):
+    model = Question
+    success_url = "/polls/"
